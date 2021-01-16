d3b10b61a8b8f0ade3debdd4b0d3fc94ee0b031c
antoniya <antoniya.vladimirova@gmail.com>
2021-01-16 19:13:59 +0200

    First exercises from advanced module

--- Advanced/python-advanced-course/01-lists-as-stacks-and-queues/lab/02-matching-brackets.py
+++
@@ -1,9 +0,0 @@
-expression = input()
-indices = []
-
-for i in range(len(expression)):
-    if expression[i] == "(":
-        indices.append(i)
-    elif expression[i] == ")":
-        start = indices.pop()
-        print(expression[start:i + 1])
